ea6e5d32dc59699d7179d90be58474cf06e4434f
SmolderTheDragon <smolderthedragon@protonmail.com>
2021-04-27 21:01:54 -0400

    Log exceptions to a file

--- reddit-npt-bot.py
+++ reddit-npt-bot.py
@@ -1,25 +1,18 @@
 import praw
 from random import randrange
+import logging
 
-# configure praw
-# authentication details are in a separate praw.ini file
-reddit =  praw.Reddit('bot1')
-reddit.validate_on_submit = True
-subreddit = reddit.subreddit("NPTtest")  # /r/NPTtest is a subreddit I made to test this bot
+# initialize logger
+logging.basicConfig(filename='status.log', level=logging.DEBUG)
 
-# grabbing which number NPT thread we are on from the disk
-with open('num.txt') as f:
-	num = f.read()
-
-# grabbing the link to the previous NPT thread from disk
-with open('prev_thread.txt') as p:
-	prev_thread = p.read()
-
-# get the id of the "Discussion" flair from the subreddit's link flairs
-for flair_template in subreddit.flair.link_templates:
-	if 'Discussion' in flair_template['text']:
-		discussion_flair_id = flair_template['id']
-		break
+# configure praw; authentication details are in a separate praw.ini file
+try:
+	reddit =  praw.Reddit('bot1')
+	reddit.validate_on_submit = True
+	subreddit = reddit.subreddit("NPTtest")  # /r/NPTtest is a subreddit I made to test this bot
+except Exception:
+	logging.exception('An error occurred when attempting to configure praw')
+	raise
 
 def generate_random_emote():
 	alphastring = 'abcdefgh'
@@ -29,11 +22,30 @@ def generate_random_emote():
 	return '[](/{0}{1}{2})'.format(table, column, row)
 
 def submit_new_NPT_post():
+	# grabbing which number NPT thread we are on from the disk
+	with open('num.txt') as f:
+		num = f.read()
+
+	# grabbing the link to the previous NPT thread from disk
+	with open('prev_thread.txt') as p:
+		prev_thread = p.read()
+
+	# get the id of the "Discussion" flair from the subreddit's link flairs
+	discussion_flair_id = None
+	for flair_template in subreddit.flair.link_templates:
+		if 'Discussion' in flair_template['text']:
+			discussion_flair_id = flair_template['id']
+			break
+	if discussion_flair_id is None:
+		logging.error('The discussion flair ID could not be found.')
+		return
+
 	# title of the NPT post
 	title = 'Pony stuff you want to talk about but isn’t worthy of a dedicated thread! #{0}'.format(num)
 
 	# text of the NPT post
 	text = "[Previous thread]({0})\n\n{1}This is the thread for any pony-related topics, thoughts, and questions you can think of that are too small to deserve their own thread. That's all you need know.".format(prev_thread, generate_random_emote())
+	text += "\n\n*This post was submitted automatically. [Source code](https://github.com/SmolderTheDragon/reddit-npt-bot).*"
 
 	# submit the thread
 	submission = subreddit.submit(title=title, selftext=text, flair_id=discussion_flair_id)
@@ -46,4 +58,8 @@ def submit_new_NPT_post():
 	with open('prev_thread.txt', 'w') as p:
 		p.write(submission.url)
 
-submit_new_NPT_post()
+try:
+	submit_new_NPT_post()
+except Exception:
+	logging.exception('An error occurred when attempting to submit the post')
+	raise
